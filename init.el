6579c1c901d797723b7b16a3b7b6b20bcbc16911
pseudo2020 <20pseudo20@gmail.com>
2026-08-06 23:25:02 +0100

    John's Emacs revision 2

--- init.el
+++
@@ -1,8 +0,0 @@
-;;; init.el --- Bootstrap -*- lexical-binding: t; -*-
-
-;; Keep customizations out of init.el
-(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
-(load custom-file 'noerror)
-
-;; Load the tangled config
-(load (expand-file-name "config.el" user-emacs-directory) nil 'nomessage)
